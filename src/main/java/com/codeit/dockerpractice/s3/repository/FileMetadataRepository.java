603b46b96465fc3b9b62917a4fcce6dc8a40709b
conradrado <gimmy6707@gmail.com>
2026-04-03 14:57:19 +0900

    찐막 이

--- src/main/java/com/codeit/dockerpractice/s3/repository/FileMetadataRepository.java
+++ src/main/java/com/codeit/dockerpractice/s3/repository/FileMetadataRepository.java
@@ -1,16 +1,17 @@
 package com.codeit.dockerpractice.s3.repository;
 
+import com.codeit.dockerpractice.s3.entity.FileMetadata;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileMetadataRepository extends JpaRepository<com.codeit.dockerpractice.domain.FileMetadata, UUID> {
+public interface FileMetadataRepository extends JpaRepository<FileMetadata, UUID> {
 
-  Optional<com.codeit.dockerpractice.domain.FileMetadata> findByS3Key(String s3Key);
+  Optional<FileMetadata> findByS3Key(String s3Key);
 
-  List<com.codeit.dockerpractice.domain.FileMetadata> findTop50ByOrderByCreatedAtDesc();
+  List<FileMetadata> findTop50ByOrderByCreatedAtDesc();
 
-  List<com.codeit.dockerpractice.domain.FileMetadata> findByS3KeyStartingWithOrderByCreatedAtDesc(String prefix);
+  List<FileMetadata> findByS3KeyStartingWithOrderByCreatedAtDesc(String prefix);
 
 }
